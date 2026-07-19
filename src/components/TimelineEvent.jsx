2d073417768fbb6427609faa483d1d79a8796f47
K-K-R-C <karlcamato145@gmail.com>
2026-07-20 05:10:26 +0800

    - Fixed images for interactive both interactive components

--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -7,6 +7,16 @@ export default function TimelineEvent({ event }) {
     return (
         <div>
 
+            <img
+                src={event.image}
+                alt={event.imageAlt}
+                style={{
+                    width: "100%",
+                    maxHeight: "300px",
+                    objectFit: "contain"
+                }}
+            />
+
             <h3>{event.year}</h3>
 
             <h4>{event.title}</h4>
